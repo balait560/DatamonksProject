d736385095474b716d85f19a23fd12ce07f3a7cb
bala <balait560@gmail.com>
2021-07-24 00:22:00 +0800

    ss

---
+++ com/test/source_data_loading.py
@@ -0,0 +1,88 @@
+import os.path
+
+import yaml
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+import com.utils.aws_utils as ut
+
+if __name__ == '__main__':
+
+    os.environ["PYSPARK_SUBMIT_ARGS"] = (
+        '--packages "mysql:mysql-connector-java:8.0.15","com.amazonaws:aws-java-sdk:1.7.4","org.apache.hadoop:hadoop-aws:2.7.4","com.springml:spark-sftp_2.11:1.1.3" pyspark-shell'
+    )
+
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("Read com.test enterprise applications") \
+        .master('local[*]') \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
+    sc = spark.sparkContext
+
+
+
+
+
+
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    print(current_dir)
+    app_config_path = os.path.abspath(current_dir  + "/../../" + "application.yml")
+    print(app_config_path)
+    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
+
+    conf = open(app_config_path)
+    app_conf = yaml.load(conf, Loader=yaml.FullLoader)
+    secret = open(app_secrets_path)
+    app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+    hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
+    hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
+    hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
+
+    src_list = app_conf['source_list']
+    for src in src_list:
+        src_config = app_conf[src]
+        stg_path = 's3a://' + app_conf['s3_conf']['s3_bucket'] + '/' + app_conf['s3_conf']['staging_location'] + '/' + src
+        print(stg_path)
+        if src == 'SB':
+            # use the ** operator/un-packer to treat a python dictionary as **kwargs
+            print("\nReading data from MySQL DB using SparkSession.read.format(),")
+            mysql_transaction_df = ut.mysql_SB_data_load(spark,app_secret,src_config)
+            mysql_transaction_df.show()
+
+
+
+            mysql_transaction_df.write.partitionBy('ins_dt').mode('overwrite').parquet(stg_path)
+
+    # SFTP source
+        elif src == 'OL':
+            print("\nReading data from SFTP  using SparkSession.read.format(),")
+            print(app_conf["OL"]["sftp_conf"]["directory"])
+            sftp_loyalty_df = ut.sftp_data_load(spark,
+                                                app_conf["OL"]["sftp_conf"]["directory"] + "/receipts_delta_GBR_14_10_2017.csv",
+                                                app_secret,app_conf["OL"]["sftp_conf"]["directory"])\
+                                                .withColumn('ins_dt', current_date())
+            sftp_loyalty_df.show(5, False)
+            sftp_loyalty_df.write.partitionBy('ins_dt').mode('overwrite').parquet(stg_path)
+
+    #MongoDB Source
+        elif src == 'CP':
+            print("\nReading data from MySQL DB using SparkSession.read.format(),")
+            mongo_customer_df = ut.mongo_data_load(spark,app_conf["mongodb_config"]["database"],
+                                                   app_conf["mongodb_config"]["collection"]
+                                                   ,app_secret,src_config)
+            mongo_customer_df.show(5,False)
+            mongo_customer_df.write.partitionBy('ins_dt').mode('overwrite').parquet()
+
+    #S3 Source
+        elif src == 'CP':
+            print("\nReading data from MySQL DB using SparkSession.read.format(),")
+            s3_campaigns_df = ut.sftp_data_load(spark,app_conf,app_secret,src_config)
+            s3_campaigns_df.show(5,False)
+            s3_campaigns_df.write.partitionBy('ins_dt').mode('overwrite').parquet()
+
+
+
+# spark-submit --packages "mysql:mysql-connector-java:8.0.15" dataframe/com.test/others/systems/mysql_df.py
---
+++ com/utils/aws_utils.py
@@ -0,0 +1,76 @@
+import yaml
+import os.path
+import com.utils.aws_utils as ut
+from pyspark.sql.functions import *
+
+
+
+def get_redshift_jdbc_url(redshift_config: dict):
+    host = redshift_config["redshift_conf"]["host"]
+    port = redshift_config["redshift_conf"]["port"]
+    database = redshift_config["redshift_conf"]["database"]
+    username = redshift_config["redshift_conf"]["username"]
+    password = redshift_config["redshift_conf"]["password"]
+    return "jdbc:redshift://{}:{}/{}?user={}&password={}".format(host, port, database, username, password)
+
+
+#JDBC connection URL for Mysql connection string
+def get_mysql_jdbc_url(mysql_config: dict):
+    host = mysql_config["mysql_conf"]["hostname"]
+    port = mysql_config["mysql_conf"]["port"]
+    database = mysql_config["mysql_conf"]["database"]
+    return "jdbc:mysql://{}:{}/{}?autoReconnect=true&useSSL=false".format(host, port, database)
+
+
+
+#Source data loading functions:
+def mysql_SB_data_load(spark,app_secret,src_config):
+    jdbc_params = {"url": ut.get_mysql_jdbc_url(app_secret),
+                   "lowerBound": "1",
+                   "upperBound": "100",
+                   "dbtable": src_config["mysql_conf"]["query"],
+                   "numPartitions": "2",
+                   "partitionColumn": src_config["mysql_conf"]["partition_column"],
+                   "user": app_secret["mysql_conf"]["username"],
+                   "password": app_secret["mysql_conf"]["password"]
+                   }
+    txnDF = spark \
+        .read.format("jdbc") \
+        .option("driver", "com.mysql.cj.jdbc.Driver") \
+        .options(**jdbc_params) \
+        .load() \
+        .withColumn('ins_dt', current_date())
+    return txnDF
+
+def sftp_data_load(spark,file_path,app_secret,file_pem):
+        print(file_path +"bala")
+        print(file_pem +"bala")
+        abs_path = os.path.abspath(file_pem) + "/" + app_secret["sftp_conf"]["pem"]
+        print(abs_path + "bala")
+        ol_txn_df = spark.read \
+            .format("com.springml.spark.sftp") \
+            .option("host", app_secret["sftp_conf"]["hostname"]) \
+            .option("port", app_secret["sftp_conf"]["port"]) \
+            .option("username", app_secret["sftp_conf"]["username"]) \
+            .option("pem", "C:\Workspace\spark.pem") \
+            .option("fileType", "csv") \
+            .option("delimiter", "|") \
+            .load("//home//ubuntu//data//receipts_delta_GBR_14_10_2017.csv")
+        return ol_txn_df
+
+def mongo_data_load(spark,dbName,collName):
+        customer_df = spark \
+            .read \
+            .format("com.mongodb.spark.sql.DefaultSource") \
+            .option("database", dbName) \
+            .option("collection",collName ) \
+            .load() \
+            .withColumn('ins_dt', current_date())
+        return customer_df
+
+def s3_data_load(spark):
+        campaign_df = spark \
+                .read \
+                .csv("s3://monksworkspace/data/KC_Extract_1_20171009.csv") \
+                .withColumn('ins_dt', current_date())
+        return campaign_df
