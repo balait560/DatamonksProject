897ebd2f89398c4e3d59de61aa241bd15e9a1b39
bala <balait560@gmail.com>
2021-07-31 15:02:17 +0800

    re

--- com/test/target_data_loading.py
+++ com/test/target_data_loading.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
                 for src in tgt_conf['source_data']:
                     file_path = "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["s3_conf"]["staging_location"] + "/" + src
                     src_df = ut.read_parquet_from_s3(spark,file_path)
-                    src_df.createOrReplaceView(src)
+                    src_df.createOrReplaceTempView(src)
                     src_df.printSchema()
                     src_df.show(5,False)
                 jdbc_url = ut.get_redshift_jdbc_url(app_secret)
@@ -88,13 +88,13 @@ if __name__ == '__main__':
                             .option("dbtable", "DATAMART.REGIS_DIM") \
                             .load()
                 REGIS_DIM.show(5,False)
-                REGIS_DIM.createTempView("REGIS_DIM")
+                REGIS_DIM.createOrReplaceTempView("REGIS_DIM")
                 print('preparing ', tgt,'data ,')
                 rtl_txf_fct_df = spark.sql(tgt_conf['loadingQuery'])
                 rtl_txf_fct_df.show()
                 ut.write_data_to_Redshift(rtl_txf_fct_df,jdbc_url,
                                           "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/temp",
-                                         tgt_conf['tablename'])
+                                         tgt_conf['tableName'])
                 print('Completed <<<<<<<<',tgt_conf['tablename'])
 
 
