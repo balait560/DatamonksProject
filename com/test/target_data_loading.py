bf045eca7678d4874e2b677568004c86f13e20f6
bala <balait560@gmail.com>
2021-07-31 12:14:22 +0800

    re

--- com/test/target_data_loading.py
+++ com/test/target_data_loading.py
@@ -3,7 +3,7 @@ import yaml
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 import com.utils.aws_utils as ut
-
+#bala
 if __name__ == '__main__':
     os.environ["PYSPARK_SUBMIT_ARGS"] = ('--packages "mysql:mysql-connector-java:8.0.15","com.amazonaws:aws-java-sdk:1.7.4","org.apache.hadoop:hadoop-aws:2.7.4","com.springml:spark-sftp_2.11:1.1.3","org.mongodb.spark:mongo-spark-connector_2.11:2.2.2" pyspark-shell')
     current_dir = os.path.abspath(os.path.dirname(__file__))
