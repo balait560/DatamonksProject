e919542ea1253695e69266b8dd47118df60a8be3
bala <balait560@gmail.com>
2021-07-31 15:06:25 +0800

    re

--- com/test/target_data_loading.py
+++ com/test/target_data_loading.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
                             .option("jdbc",jdbc_url) \
                             .option("tempdir", "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/temp") \
                             .option("forward_spark_s3_credentials", "true") \
-                            .option("dbtable", "DATAMART.REGIS_DIM") \
+                            .option("dbtable", "REGIS_DIM") \
                             .load()
                 REGIS_DIM.show(5,False)
                 REGIS_DIM.createOrReplaceTempView("REGIS_DIM")
