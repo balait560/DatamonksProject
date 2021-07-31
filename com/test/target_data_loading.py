17b1e43bb3fb3aed5ceb439720724d19de7ff13f
bala <balait560@gmail.com>
2021-07-31 12:35:28 +0800

    re

--- com/test/target_data_loading.py
+++ com/test/target_data_loading.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     os.environ["PYSPARK_SUBMIT_ARGS"] = ('--packages "mysql:mysql-connector-java:8.0.15","com.amazonaws:aws-java-sdk:1.7.4","org.apache.hadoop:hadoop-aws:2.7.4","com.springml:spark-sftp_2.11:1.1.3","org.mongodb.spark:mongo-spark-connector_2.11:2.2.2" pyspark-shell')
     current_dir = os.path.abspath(os.path.dirname(__file__))
     app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
-    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
     conf = open(app_config_path)
     app_conf = yaml.load(conf,Loader=yaml.FullLoader)
     secret = open(app_secrets_path)
